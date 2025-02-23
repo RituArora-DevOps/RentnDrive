ac5003e5e9f986c842e98f4084eede04754e421b
RArora <130247456+RituArora-DevOps@users.noreply.github.com>
2025-02-23 13:38:41 -0500

    Update mysql-script.sql

--- mysql-script.sql
+++ mysql-script.sql
@@ -16,53 +16,12 @@ CREATE TABLE cars (
     make VARCHAR(255) NOT NULL,
     model VARCHAR(255) NOT NULL,
     year INT NOT NULL,
+    type VARCHAR(255) NOT NULL,
     price_per_day DECIMAL(10, 2) NOT NULL,
     status ENUM('available', 'booked', 'maintenance') NOT NULL
 );
 
-
--- 3. Bookings Table
-CREATE TABLE bookings (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    car_id INT,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    total_amount DECIMAL(10, 2) NOT NULL,
-    status ENUM('pending', 'confirmed', 'cancelled', 'completed') NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (car_id) REFERENCES cars(id)
-);
-
--- 4. Payments Table
-CREATE TABLE payments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    booking_id INT,
-    amount DECIMAL(10, 2) NOT NULL,
-    payment_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    payment_method ENUM('credit_card', 'debit_card', 'paypal', 'bank_transfer') NOT NULL,
-    status ENUM('pending', 'completed', 'failed') NOT NULL,
-    FOREIGN KEY (booking_id) REFERENCES bookings(id)
-);
-
--- Drop fk for bookings and payments
-ALTER TABLE payments DROP FOREIGN KEY payments_ibfk_1;
-ALTER TABLE bookings DROP FOREIGN KEY bookings_ibfk_1;
-ALTER TABLE bookings DROP FOREIGN KEY bookings_ibfk_2;
-
--- Droping seperate tables of bookings and payments
-DROP TABLE IF EXISTS bookings;
-DROP TABLE IF EXISTS payments;
-
--- alter table car to add type field
-ALTER TABLE cars
-ADD COLUMN type VARCHAR(255) NOT NULL;
-
-
--- Create new table booking_payments with extra field
-
+-- 3. Booking-payment Table
 CREATE TABLE booking_payments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
