9bae34b5da61cfafebdf743f0aa5e334848d4eef
Hitao1623 <hollywang23@hotmail.com>
2025-02-23 21:23:21 -0500

    database changed

--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ const log = require("npmlog");
 
 log.level = process.env.LOG_LEVEL || "info"; // Default to "info" if LOG_LEVEL is not set
 
-log.heading = "CarRentalSystem"; // Custom heading for log entries
+log.heading = "rentndrive"; // Custom heading for log entries
 log.addLevel("trace", 1000, { fg: "blue", bold: true }); // Adding a custom log level (optional)
 
 log.addLevel("notice", 2000, { fg: "green", bold: true }); // Custom log level 'notice'
