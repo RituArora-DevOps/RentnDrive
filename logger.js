73d05482bb48e7db47d9c37f76e22a7399553d11
Hitao1623 <hollywang23@hotmail.com>
2025-07-16 22:34:48 -0400

    logger path updated for deployment

--- logger.js
+++ logger.js
@@ -2,27 +2,26 @@ const log = require("npmlog");
 const fs = require("fs");
 const path = require("path");
 
-
 log.level = process.env.LOG_LEVEL || "info";
 log.heading = "CarRentalSystem";
 log.addLevel("trace", 1000, { fg: "blue", bold: true });
 log.addLevel("notice", 2000, { fg: "green", bold: true });
 
-const logFilePath = path.join(__dirname, "./app/logs/app.log"); 
+const isProduction = process.env.NODE_ENV === "production";
+const logFilePath = isProduction ? "/tmp/app.log" : path.join(__dirname, "./app/logs/app.log");
 
 const originalLog = log.log;
 
 log.log = function (level, prefix, message, ...args) {
-    originalLog.call(this, level, prefix, message, ...args);
+  originalLog.call(this, level, prefix, message, ...args);
 
-    const logMessage = `${new Date().toISOString()} - ${level.toUpperCase()} - ${prefix} - ${message} ${args.join(" ")}\n`;
+  const logMessage = `${new Date().toISOString()} - ${level.toUpperCase()} - ${prefix} - ${message} ${args.join(" ")}\n`;
 
-    fs.appendFile(logFilePath, logMessage, (err) => {
-        if (err) {
-            console.error("Error writing to log file:", err);
-        }
-    });
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error("Error writing to log file:", err);
+    }
+  });
 };
 
 module.exports = log;
-
